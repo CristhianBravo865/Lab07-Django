eb4b11472851bbb66006b38fadd70743ae394c08
Cristhian <cristhian.bravo.8657@gmail.com>
2025-06-02 15:11:41 -0500

    Se añaden rutas para listar, editar y eliminar destinos turísticos en urls.py

--- projects/destinos_project/travello/urls.py
+++ projects/destinos_project/travello/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_destination, name='add_destination'),
+    path('list/', views.list_destinations, name='list_destinations'),
+    path('edit/<int:id>/', views.edit_destination, name='edit_destination'),
+    path('delete/<int:id>/', views.delete_destination, name='delete_destination'),
 ]
