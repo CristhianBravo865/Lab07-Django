6aad796605d9cdb2433ada7aaad8510213f6a4fd
Cristhian <cristhian.bravo.8657@gmail.com>
2025-06-02 09:55:56 -0500

    Se agrega la vista del projecto

--- projects/destinos_project/travello/views.py
+++ projects/destinos_project/travello/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from .models import Destination
+from .models import DestinosTuristicos
 # Create your views here.
 
 
 def index(request):
-
-    dests = Destination.objects.all()
-
-    return render(request, "index.html", {'dests': dests})
+    destinos = DestinosTuristicos.objects.all()
+    return render(request, 'index.html', {'destinos': destinos})
