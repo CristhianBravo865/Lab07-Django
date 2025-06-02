94b91be0164926061bd92f11bfa18d60d176ff5f
Cristhian <cristhian.bravo.8657@gmail.com>
2025-06-02 14:56:42 -0500

    Se crea la vista de add_destination

--- projects/destinos_project/travello/views.py
+++ projects/destinos_project/travello/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import DestinosTuristicos
 # Create your views here.
 
@@ -6,3 +6,22 @@ from .models import DestinosTuristicos
 def index(request):
     destinos = DestinosTuristicos.objects.all()
     return render(request, 'index.html', {'destinos': destinos})
+def add_destination(request):
+    if request.method == 'POST':
+        nombreCiudad = request.POST.get('nombreCiudad')
+        descripcionCiudad = request.POST.get('descripcionCiudad')
+        imagenCiudad = request.FILES.get('imagenCiudad')
+        precioTour = request.POST.get('precioTour')
+        ofertaTour = request.POST.get('ofertaTour') == 'on'
+
+        destino = DestinosTuristicos(
+            nombreCiudad=nombreCiudad,
+            descripcionCiudad=descripcionCiudad,
+            imagenCiudad=imagenCiudad,
+            precioTour=precioTour,
+            ofertaTour=ofertaTour
+        )
+        destino.save()
+        return redirect('list_destinations')
+
+    return render(request, 'add_destination.html')
